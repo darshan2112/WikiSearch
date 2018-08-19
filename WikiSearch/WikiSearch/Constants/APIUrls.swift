6cbf3960acb9464951358fb1d3eb35f4007a59c6
Darshan Shivakumar <darshan.ks@onmobile.com>
2018-08-19 17:24:14 +0530

    Adding APIUrls as constants file

---
+++ WikiSearch/WikiSearch/Constants/APIUrls.swift
@@ -0,0 +1,16 @@
+//
+//  APIUrls.swift
+//  WikiSearch
+//
+//  Created by Darshan K S on 18/08/18.
+//  Copyright © 2018 Darshan K S. All rights reserved.
+//
+
+import Foundation
+
+struct APIurls {
+    
+    static let BASE_URL = "https://en.wikipedia.org/w/api.php"
+    static let WEB_PAGE_BASE_URL = "https://en.wikipedia.org/wiki/"
+
+}
